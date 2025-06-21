656d6a23d9093285070198a51d67999cb3a1c439
CharanSuggala26 <suggalasaicharan789@gmail.com>
2025-06-21 12:46:10 +0530

    Backend has been setup

---
+++ backend/Middlewares/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt=require("jsonwebtoken");
+require('dotenv').config
+
+const verifyToken=(req,res,next)=>{
+    let bearerToken=req.body.headers.authorization;
+    if(!bearerToken){
+        return res.send({message:"Unauthorized access..pls login"})
+    }
+
+    let token=bearerToken.split(' ')[1];
+    try{
+            let decodedToken=jwt.verify(token,process.env.SECRET_KEY);
+            next();
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports=verifyToken
