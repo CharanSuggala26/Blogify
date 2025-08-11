feceb3e7f1c5102c890b14f51abc52982938e157
CharanSuggala26 <suggalasaicharan789@gmail.com>
2025-08-11 21:57:54 +0530

    Created database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,30 @@
 const exp=require('express')
 require('dotenv').config()
+const cors=require('cors')
+const {MongoClient}=require('mongodb')
+
 
 const app=exp()
 const port=process.env.PORT
+
+app.use(cors())
+//app.use(express.json())
+
+const client = new MongoClient(process.env.DB_URI);
+
+client
+.connect()
+.then(()=>{
+    const db=client.db("blogDB");
+    app.set("bloggerCollection",db.collection("blogger"));
+    app.set("blogCollection",db.collection("blog"));
+    app.set("commentCollection",db.collection("comment"));
+    console.log("DB connected successfully")
+})
+.catch((error)=>{
+    console.error("Error connecting database: ",error);
+});
+
 
 app.listen(port,()=>{
     console.log(`server is running on port : ${port}`);
--- my-react-tailwind/src/Redux/store.jsx
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import userLoginReducer from './slices/userLoginSlice'
-
-export const reduxStore=configureStore({
-    reducer:{
-        userLogin:userLoginReducer
-    }
-})
